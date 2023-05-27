d7e82bc61cd05421145b0cca6f38a2f0c5656658
k4im <contato.vitorsantos@hotmail.com>
2023-05-27 16:26:22 -0300

    Adicionado teste CPF

--- app/Services/Shared/shared.models/ValueObjects/CadastroPessoaFisica.cs
+++ app/Services/Shared/shared.models/ValueObjects/CadastroPessoaFisica.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using shared.models.Exceptions;
 
 namespace shared.models.ValueObjects
 {
@@ -16,8 +17,8 @@ namespace shared.models.ValueObjects
 
         public string ValidarCpf(string cpf)
         {
-            if (!Regex.IsMatch(cpf, "^[0-9]*$")) throw new Exception("Insira apenas os numeros do cpf");
-            if(string.IsNullOrEmpty(cpf)) throw new Exception("O cpf não pode estar nulo!");
+            if (!Regex.IsMatch(cpf, "^[0-9]*$")) throw new CaracterInvalido("Insira apenas os numeros do cpf");
+            if(string.IsNullOrEmpty(cpf)) throw new CampoVazio("O cpf não pode estar nulo!");
             if(cpf.Length < 11) throw new Exception("O cpf deve conter no minimo 11 caracteres!");
             return cpf.Trim();
         }
