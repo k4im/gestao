230eae56843be90d88880e8445552c598b718ea3
k4im <contato.vitorsantos@hotmail.com>
2023-05-27 15:51:29 -0300

    Adicionado teste repo pessoa

--- app/Services/autenticacao/autenticacao.service.tests/RepoPessoaTests.cs
+++ app/Services/autenticacao/autenticacao.service.tests/RepoPessoaTests.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
-
 namespace autenticacao.service.tests
 {
     public class RepoPessoaTests
