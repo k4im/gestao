541f7ce6c4467fa34a358ca073a13e30ba19c918
k4im <contato.vitorsantos@hotmail.com>
2023-06-20 12:36:07 -0300

    Removido dependencia sharedModels

--- app/Services/autenticacao/autenticacao.service/Models/Pessoa.cs
+++ app/Services/autenticacao/autenticacao.service/Models/Pessoa.cs
@@ -2,7 +2,7 @@ namespace autenticacao.service.Models
 {
     public class Pessoa
     {
-        protected Pessoa() {}
+        protected Pessoa() { }
         public Pessoa(Nome nome, Endereco endereco, Telefone telefone, CadastroPessoaFisica cpf)
         {
             Nome = nome;
@@ -12,12 +12,12 @@ namespace autenticacao.service.Models
         }
         [Key]
         public int Id { get; private set; }
-        public Nome Nome { get;}
-        public Endereco Endereco { get;}
-        public Telefone Telefone { get;}
-        public CadastroPessoaFisica Cpf {get;}
+        public Nome Nome { get; }
+        public Endereco Endereco { get; }
+        public Telefone Telefone { get; }
+        public CadastroPessoaFisica Cpf { get; }
 
-    
+
         public void mudarEndereco(Endereco novoEndereco)
         {
             Endereco.atualizarEndereco(novoEndereco);
---
+++ app/Services/autenticacao/autenticacao.service/Models/ValueObjects/Nome.cs
@@ -0,0 +1,30 @@
+namespace autenticacao.service.Models.ValueObjects
+{
+    [Keyless]
+    public class Nome
+    {
+        public Nome(string primeiroNome, string sobreNome)
+        {
+            PrimeiroNome = validarPrimeiroNome(primeiroNome);
+            SobreNome = validarSobreNome(sobreNome);
+        }
+        protected Nome() { }
+
+        public string PrimeiroNome { get; }
+        public string SobreNome { get; }
+
+        string validarPrimeiroNome(string nome)
+        {
+            if (string.IsNullOrEmpty(nome)) throw new CampoVazio("O nome não pode estar vazio!");
+            if (!Regex.IsMatch(nome, @"^[a-zA-Z ]+$")) throw new CaracterInvalido("O nome não pode conter caracteres especiais");
+            return nome;
+        }
+
+        string validarSobreNome(string sobreNome)
+        {
+            if (string.IsNullOrEmpty(sobreNome)) throw new CampoVazio("O sobrenome não pode estar vazio!");
+            if (!Regex.IsMatch(sobreNome, @"^[a-zA-Z ]+$")) throw new CaracterInvalido("O sobrenome não pode conter caracteres especiais");
+            return sobreNome.Trim();
+        }
+    }
+}
